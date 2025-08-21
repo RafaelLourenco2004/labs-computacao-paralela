ceb308dbfecfd44fa3236c523e875be942252c13
Rafael <115298880+RafaelLourenco2004@users.noreply.github.com>
2025-08-21 23:37:44 +0000

    lab 1 e 2

---
+++ computacao_paralela/lab1/exe2.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/wait.h>
+
+int main(){
+    pid_t pid;
+    int n = 4;
+    for (int i=0; i<n; i++){
+        pid = fork();
+
+        if (pid == 0){
+            printf("Eu sou o filho, meu pid é %d e o do meu pai é %d\n", getpid(), getppid());
+            return 0;
+        }
+
+        if (pid < 0){
+            printf("Fork falhou.");;
+            return -1;
+        }
+    }
+    
+    for (int i=0; i<n; i++)
+        wait(NULL);
+
+    printf("Eu sou o pai e todos os meus filhos terminaram.");
+
+    return 0;
+}
